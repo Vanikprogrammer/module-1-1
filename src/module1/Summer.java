51150c4cd8c065f04d503eb83b609bf64eb9a883
Ivan <ngk-trade@mail.ru>
2017-06-28 13:49:58 +0300

    выводим текст на консоль

---
+++ src/module1/Summer.java
@@ -0,0 +1,10 @@
+package module1;
+
+/**
+ * Created by 1 on 28.06.2017.
+ */
+public class Summer {
+    public static void main(String[] args) {
+        System.out.println("Обучаясь джава!");
+    }
+}
