b371ef85c57873edd344c531c7d953cb315ba9ac
Ivan <ngk-trade@mail.ru>
2017-06-28 13:48:04 +0300

    Выводим текст на экран

--- src/Module1.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by 1 on 25.06.2017.
- */
-public class Module1 {
-    public static void main(String[] args) {
-        System.out.println("Вопрос: Как я проведу лето?" + "\n" + "Ответ: не ожидал, что я это скажу, но я буду учиться!!!)))");
-    }
-}
