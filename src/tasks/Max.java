9f73ae7308a9d17088169807bada17401980e625
Ivan <ngk-trade@mail.ru>
2017-07-01 14:06:26 +0300

    максимальное из четырех

--- src/tasks/Max.java
+++ src/tasks/Max.java
@@ -11,13 +11,31 @@ import java.util.Scanner;
 public class Max {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Введите четыре числа:");
-        int [] ch = new int[4];
-        for(int i = 0; i < ch.length; i++){
-            ch[i] = sc.nextInt();
-        }
-        Arrays.sort(ch);
-        System.out.println(ch[3]);
-    }
 
+        int ch[] = new int[4];
+        System.out.println("Введите четыре числа:");
+        for (int i = 0; i < ch.length; i++) {
+
+            try {
+                ch[i] = sc.nextInt();
+            } catch (Exception e) {
+                System.out.println("Вы ввели не число!!!");
+                System.out.println("Введите число!!!");
+                i--;
+                sc.nextLine();
+            }
+        }
+
+
+        int max = ch[0];
+        for (int j = 0; j < ch.length; j++) {
+            if (ch[j] > max) {
+                max = ch[j];
+            }
+        }
+
+        System.out.println(max);
+    }
 }
+
+
